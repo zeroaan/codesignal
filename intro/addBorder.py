eef39ea9d28a269037587720743093a09e395ab3
Jinyoung Ahn <yj6210ag@gmail.com>
2020-04-29 15:36:23 +0900

    pass

---
+++ intro/addBorder.py
@@ -0,0 +1,15 @@
+def addBorder(picture):
+    result = []
+    col = len(picture[0]) + 2
+    row = len(picture) + 2
+    result.append('*' * col)
+
+    for i in range(len(picture)):
+        value = '*'
+        for j in range(len(picture[i])):
+            value  += picture[i][j]
+        value += '*'
+        result.append(value)
+
+    result.append('*' * col)
+    return result
