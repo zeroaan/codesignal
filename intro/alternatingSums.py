45dc1c91a1279cb5e886452c32ad7e70b109f833
Jinyoung Ahn <yj6210ag@gmail.com>
2020-04-29 15:12:51 +0900

    pass

---
+++ intro/alternatingSums.py
@@ -0,0 +1,9 @@
+def alternatingSums(a):
+    team_1, team_2 = 0, 0
+    
+    for i in range(len(a)):
+        if i % 2 == 0:
+            team_1 += a[i]
+        else: team_2 += a[i]
+
+    return [team_1, team_2]
