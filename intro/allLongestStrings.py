8d647287dc6c8f54db78649aa5bc6d9a6ddf422a
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-08 14:52:12 +0900

    pass

--- intro/allLongestStrings.py
+++ intro/allLongestStrings.py
@@ -1,9 +1,11 @@
 def allLongestStrings(inputArray):
-    value = []
-    result = []
-    for i in inputArray:
-        value.append(len(i))
-    for j in range(len(inputArray)):
-        if max(value) == value[j]:
-            result.append(inputArray[j])
-    return result
+    # value, result = [], []
+    # for i in inputArray:
+    #     value.append(len(i))
+    # for j in range(len(inputArray)):
+    #     if max(value) == value[j]:
+    #         result.append(inputArray[j])
+    # return result
+
+    longest = max([len(i) for i in inputArray])
+    return [j for j in inputArray if len(j) == longest]
