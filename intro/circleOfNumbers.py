1d2489a6ce895c679d052506bb1b4f313b4d02c9
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-09 16:01:38 +0900

    pass

---
+++ intro/circleOfNumbers.py
@@ -0,0 +1,6 @@
+def circleOfNumbers(n, firstNumber):
+    # if firstNumber < n//2:
+    #     return firstNumber + n//2
+    # return firstNumber - n//2
+
+    return firstNumber + n//2 if firstNumber < n//2 else firstNumber - n//2
