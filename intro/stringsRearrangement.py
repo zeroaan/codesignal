fe0678d35508b59b84d19658d916f8490a1af6d7
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-09 23:03:36 +0900

    not not pass

---
+++ intro/stringsRearrangement.py
@@ -0,0 +1,7 @@
+"""
+Given an array of equal-length strings, you'd like to know if it's possible to rearrange the order of the elements in such a way that each consecutive pair of strings differ by exactly one character. Return true if it's possible, and false if not.
+"""
+
+
+def stringsRearrangement(inputArray):
+
