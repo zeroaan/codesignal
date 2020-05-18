c3fb00661aea2af7eb0798910e9319388671abf1
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-18 13:42:00 +0900

    pass

--- intro/sortbyHeight.py
+++ intro/sortbyHeight.py
@@ -1,14 +1,12 @@
 def sortByHeight(a):
-    copy_a = sorted(a)
-    while -1 in copy_a:
-        copy_a.remove(-1)
+    height = [i for i in a if i > 0]
     
     result = []
     for i in range(len(a)):
         if a[i] == -1:
             result.append(-1)
         else:
-            result.append(min(copy_a))
-            copy_a.remove(min(copy_a))
+            result.append(min(height))
+            height.remove(min(height))
 
     return result
