34e2da0595563d1b04253bb2eadd9357ef7a1920
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-19 17:36:10 +0900

    pass

--- intro/almostIncreasingSequence.py
+++ intro/almostIncreasingSequence.py
@@ -1,26 +1,19 @@
-"""
-Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
-
-Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
-
-Example
-
-For sequence = [1, 3, 2, 1], the output should be
-almostIncreasingSequence(sequence) = false.
-
-There is no one element in this array that can be removed in order to get a strictly increasing sequence.
-
-For sequence = [1, 3, 2], the output should be
-almostIncreasingSequence(sequence) = true.
-
-You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
-"""
-
 def almostIncreasingSequence(sequence):
-    a = 0
-    for i in range(len(sequence)-1):
-        if sequence[i]>=sequence[i+1]:
-            a += 1
-    if a > 1:
-        return False
+    # a = 0
+    # for i in range(len(sequence)-1):
+    #     if sequence[i]>=sequence[i+1]:
+    #         a += 1
+    # if a > 1:
+    #     return False
+    # return True  
+
+    count_decreasing_sq = 0
+    for i in range(len(sequence) - 1):
+        if sequence[i+1] <= sequence[i]:
+            count_decreasing_sq += 1
+            if (i >= 1) and (sequence[i+1] <= sequence[i-1]):
+                if (len(sequence) - 2 > i) and (sequence[i+2] <= sequence[i]):
+                    count_decreasing_sq += 1
+        if count_decreasing_sq > 1:
+            return False
     return True
