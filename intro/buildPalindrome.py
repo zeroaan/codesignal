98715cfc4d2a9b55e750ecfd9adc7a118439cda4
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-22 14:26:05 +0900

    pass

--- intro/buildPalindrome.py
+++ intro/buildPalindrome.py
@@ -1,9 +1,5 @@
-
 def buildPalindrome(st):
-    result = []
     for i in range(len(st)):
         palindrome = st + st[0:i][::-1]
         if palindrome == palindrome[::-1]:
-            result.append(palindrome)
-            
-    return min(result, key=len)
+            return palindrome
