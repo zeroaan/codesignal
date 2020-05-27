56296d3cef4a8b405ea3c56bb8fcd3d002b91188
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-27 16:15:21 +0900

    pass

--- intro/alternatingSums.py
+++ intro/alternatingSums.py
@@ -1,9 +1,11 @@
 def alternatingSums(a):
-    team_1, team_2 = 0, 0
+    # team_1, team_2 = 0, 0
     
-    for i in range(len(a)):
-        if i % 2 == 0:
-            team_1 += a[i]
-        else: team_2 += a[i]
+    # for i in range(len(a)):
+    #     if i % 2 == 0:
+    #         team_1 += a[i]
+    #     else: team_2 += a[i]
 
-    return [team_1, team_2]
+    # return [team_1, team_2]
+
+    return [sum(a[::2]), sum(a[1::2])]
--- intro/areSimilar.py
+++ intro/areSimilar.py
@@ -8,5 +8,11 @@ def areSimilar(a, b):
         if a[i] != b[i]:
             value.append(i)
             
+    # 13 ~ 14줄은 없어도 문제 통과는 되지만
+    # 만약 a = [1, 2, 2], b = [2, 2, 2] 일 경우
+    # 16줄에서 인덱스 에러가 나기 때문에 해당 줄을 추가해주었다.
+    if len(value) != 2:
+        return False
+    
     a[value[0]], a[value[1]] = a[value[1]], a[value[0]]
     return a == b
