2ac75f7534d9966f836209effe37159ed849b26e
Jinyoung Ahn <yj6210ag@gmail.com>
2020-05-28 11:58:05 +0900

    pass

--- intro/sudoku.py
+++ intro/sudoku.py
@@ -3,25 +3,25 @@ def sudoku(grid):
     row = len(grid[0])
 
     # 가로가 만족하지 않을 경우 False
-    for g in grid:
-        if sorted(list(set(g))) != sorted(g):
+    for r in grid:
+        if sorted(list(set(r))) != sorted(r):
             return False
 
     # 세로가 만족하지 않을 경우 False
-    for j in range(row):
-        a = []
-        for i in range(col):
-            a.append(grid[i][j])
-        if sorted(list(set(a))) != sorted(a):
+    for i in range(row):
+        c = []
+        for j in range(col):
+            c.append(grid[j][i])
+        if sorted(list(set(c))) != sorted(c):
             return False
 
     # 3 X 3이 만족하지 않을 경우 False
     for i in range(0, col, 3):
         for j in range(0, row, 3):
-            b = [grid[i][j], grid[i][j+1], grid[i][j+2], \
+            g = [grid[i][j], grid[i][j+1], grid[i][j+2], \
                 grid[i+1][j], grid[i+1][j+1], grid[i+1][j+2], \
                 grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]
-            if sorted(list(set(b))) != sorted(b):
+            if sorted(list(set(g))) != sorted(g):
                 return False
 
     return True
